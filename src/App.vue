e2a93aa9fd49d7bc6e31d9fd5c57a7c961dd2946
tmakic <tomakichouen@gmail.com>
2021-09-26 01:01:12 +0900

    add: APIで情報を取得する機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template lang='pug'>
   #app
-    img(src="./assets/logo.png")
-    HelloWorld
+    h2 都道府県情報のテスト
+    p {{ prefectureList[0] }}
+
+    h2 人口情報のテスト
+    p {{ totalPopulation.label }}
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-import HelloWorld from './components/HelloWorld'
+import { defineComponent, onMounted, reactive, toRefs } from '@vue/composition-api'
+import axiosBase from 'axios'
 
 export default defineComponent({
   name: 'App',
 
-  components: {
-    HelloWorld
+  setup () {
+    const axios = axiosBase.create({
+      baseURL: process.env.RESAS_API_BASE_URL,
+      headers: {
+        'X-API-KEY': process.env.RESAS_API_KEY
+      }
+    })
+
+    /**
+     * 都道府県情報を取得
+     */
+    const getPrefectureData = () => {
+      axios.get('api/v1/prefectures').then(response => {
+        state.prefectureList = response.data.result
+      })
+    }
+
+    /**
+     * 人口情報を取得
+     */
+    const getPopulationData = prefCode => {
+      axios.get('/api/v1/population/composition/perYear', {
+        params: {
+          prefCode: prefCode,
+          cityCode: '-'
+        }
+      }).then(response => {
+        state.totalPopulation = response.data.result.data[0]
+      })
+    }
+
+    onMounted(() => {
+      getPrefectureData()
+      getPopulationData(11)
+    })
+
+    const state = reactive({
+      // 都道府県情報
+      prefectureList: [],
+      // 総人口情報
+      totalPopulation: []
+    })
+
+    return { ...toRefs(state) }
   }
 })
 </script>
