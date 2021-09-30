3288558b42065ed18e16fb8838e1aafc2426e8b2
tmaki <76893560+tmakic@users.noreply.github.com>
2021-09-30 18:51:52 +0900

    fix/

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,4 @@
 "use strict";
 module.exports = {
-  NODE_ENV: '"production"',
-  RESAS_API_KEY: process.env.RESAS_API_KEY,
-  RESAS_API_BASE_URL: process.env.RESAS_API_BASE_URL
+  NODE_ENV: '"production"'
 };
